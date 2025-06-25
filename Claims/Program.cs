ecb2bb4952af55dc6658fa9bafbd56eb7710885a
Arseny Garelyshev <monstrag@gmail.com>
2025-06-25 18:48:03 +0200

    refactor: add serilog

--- Claims/Program.cs
+++ Claims/Program.cs
@@ -5,73 +5,91 @@ using Claims.Claims;
 using Claims.Claims.Models;
 using Microsoft.EntityFrameworkCore;
 using MongoDB.Driver;
+using Serilog;
 using Testcontainers.MongoDb;
 using Testcontainers.MsSql;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
 
-// Start Testcontainers for SQL Server and MongoDB
-var sqlContainer = (
-	RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
-		? new MsSqlBuilder().WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-		: new MsSqlBuilder()
-).Build();
+try
+{
+	var builder = WebApplication.CreateBuilder(args);
 
-var mongoContainer = new MongoDbBuilder().WithImage("mongo:latest").Build();
+	builder.Services.AddSerilog();
 
-var sqlContainerStartTask = sqlContainer.StartAsync();
-var mongoContainerStartTask = mongoContainer.StartAsync();
+	// Start Testcontainers for SQL Server and MongoDB
+	var sqlContainer = (
+		RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+			? new MsSqlBuilder().WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+			: new MsSqlBuilder()
+	).Build();
 
-await Task.WhenAll(sqlContainerStartTask, mongoContainerStartTask);
+	var mongoContainer = new MongoDbBuilder().WithImage("mongo:latest").Build();
 
-// Add services to the container.
-builder
-	.Services.AddControllers()
-	.AddJsonOptions(x =>
+	var sqlContainerStartTask = sqlContainer.StartAsync();
+	var mongoContainerStartTask = mongoContainer.StartAsync();
+
+	await Task.WhenAll(sqlContainerStartTask, mongoContainerStartTask);
+
+	// Add services to the container.
+	builder
+		.Services.AddControllers()
+		.AddJsonOptions(x =>
+		{
+			x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+		});
+
+	builder.Services.AddDbContext<AuditContext>(options =>
+		options.UseSqlServer(sqlContainer.GetConnectionString())
+	);
+
+	builder.Services.AddDbContext<ClaimsContext>(options =>
 	{
-		x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+		var client = new MongoClient(mongoContainer.GetConnectionString());
+		var database = client.GetDatabase(builder.Configuration["MongoDb:DatabaseName"]); // Use a default/test database name
+		options.UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName);
 	});
 
-builder.Services.AddDbContext<AuditContext>(options =>
-	options.UseSqlServer(sqlContainer.GetConnectionString())
-);
+	// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ClaimsContext>(options =>
-{
-	var client = new MongoClient(mongoContainer.GetConnectionString());
-	var database = client.GetDatabase(builder.Configuration["MongoDb:DatabaseName"]); // Use a default/test database name
-	options.UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName);
-});
+	builder.Services.AddScoped<Auditer>();
+	builder.Services.AddScoped<Repository<Claim>>();
+	builder.Services.AddScoped<Repository<Cover>>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+	var app = builder.Build();
 
-builder.Services.AddScoped<Auditer>();
-builder.Services.AddScoped<Repository<Claim>>();
-builder.Services.AddScoped<Repository<Cover>>();
+	// Configure the HTTP request pipeline.
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
 
-var app = builder.Build();
+	app.UseSerilogRequestLogging();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	using (var scope = app.Services.CreateScope())
+	{
+		var context = scope.ServiceProvider.GetRequiredService<AuditContext>();
+		context.Database.Migrate();
+	}
+
+	app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
+catch (Exception ex)
 {
-	var context = scope.ServiceProvider.GetRequiredService<AuditContext>();
-	context.Database.Migrate();
+	Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-app.Run();
+finally
+{
+	Log.CloseAndFlush();
+}
 
 public partial class Program;
